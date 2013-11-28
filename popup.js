0a6b9758616dffd3fe239aff982d90b12227f4f8
Dinkins <nic.dinkins@gmail.com>
2013-11-28 11:59:32 -0500

    removing uncessary hashmap implementation, old files

--- popup.js
+++ popup.js
@@ -1,43 +1,31 @@
-var record = true; 
-var parent = chrome.contextMenus.create({"id": String(Date.now()), "title": "Write CSS Changes To File"});
-chrome.tabs.executeScript(null, {file: "track.js"});
+chrome.contextMenus.create({"id": String(Date.now()), "title": "Write CSS Changes To File"});
+
 
 chrome.browserAction.onClicked.addListener(function(tab) {
-	if(record)
-	{		
-		toggleNodeMonitoring("START");
-		console.log(record);
-		record = false; 
-	}
-	else
-	{
-		toggleNodeMonitoring("STOP");
-		console.log("stopped");
-		record = true; 
-	}
-});
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+	toggleNodeMonitoring("TRIGGER OBSERVER");
+});});
 
-//listen for notifiication to write out file
+
+
+// listen for notifiication to write out file
 chrome.contextMenus.onClicked.addListener(function ()
 {
-  		//send a message to write the file out and return it back 
-  		toggleNodeMonitoring("WRITE");
+  		toggleNodeMonitoring("GET CHANGES");
 });
 
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-    console.log(sender.tab ?
-                "from a content script:" + sender.tab.url :
-                "from the extension");
-    if (request.greeting == "write")
-      sendResponse({farewell: "goodbye"});
+  	console.log(request);
   });
 
 //helper methods
 function toggleNodeMonitoring(message)
-{
-	console.log('method called');
-	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-			chrome.tabs.sendMessage(tabs[0].id, {greeting: message});
-		});
+{ 
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs)
+	{
+		 chrome.tabs.sendMessage(tabs[0].id, {greeting: message}, function(response) {
+		 	//console.log(response);
+		 });
+	});	
 }
--- track.js
+++ track.js
@@ -1,39 +1,41 @@
 var iterated = false; 
-var change_map = new Map();
+var change_map = {};
 var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
 var myObserver = new MutationObserver (mutationHandler);
 var att_filter = new Array ('style'); //need to filter style changes only
 var obsConfig  = {childList: false, characterData: false, attributes: true, subtree: false, attributeFilter: att_filter};
 var page_target = document.querySelectorAll('body *');
-
-console.log('injected');
-alert('injected');
+var record = true; 
 
 //listen for start/stop tracking message from the extension
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) 
-  {
-    console.log('received a message');
-    if(request.greeting === "STOP")
-    {
-      Array.prototype.slice.call(page_target).forEach(function(target, index, arr) {
-        myObserver.disconnect();
-      });
-    }
-    else if(request.greeting === "START")
-    {
-      Array.prototype.slice.call(page_target).forEach(function(target, index, arr) {
-        myObserver.observe(target, obsConfig);
-      });
-      console.log("started");
-    }
-    else if(request.greeting === "WRITE")
-    {
-      createStringFromHashMap();
-    }
-  }
-);
 
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse)
+{
+      if(request.greeting === "TRIGGER OBSERVER")
+      {
+        if (record)
+        {
+          //start monitoring
+          Array.prototype.slice.call(page_target).forEach(function(target, index, arr) {
+          myObserver.observe(target, obsConfig);
+          });
+          record = false; 
+        }
+        else
+        {
+          //stop just fucking stop
+          Array.prototype.slice.call(page_target).forEach(function(target, index, arr) {
+          myObserver.disconnect();
+           });
+          record = true; 
+        }
+      }
+      if(request.greeting === "GET CHANGES")
+      {
+        console.log(createStringFromHashMap());
+        sendResponse({farewell: createStringFromHashMap()});
+      }
+});
 
 
 function mutationHandler (mutationRecords) 
@@ -43,13 +45,12 @@ function mutationHandler (mutationRecords)
       var target = mutation.target; 
       if(target.id)
       {
-        change_map.put(target.id, target.getAttribute("style"));
+        change_map["#" + target.id] =target.getAttribute("style");
       }
       else
       {
-        var path = "body," + createNodePath(target, "");
-        change_map.put(killTrailingComma(path), target.getAttribute("style"));
-        
+        var path = killTrailingComma("body," + createNodePath(target, ""));
+        change_map[path] = target.getAttribute("style");
       }
       console.log(change_map);
   });
@@ -71,7 +72,7 @@ function createNodePath(node, constructed_path)
       }
       else
       {
-        constructed_path = assemblePathString(loop_node.localName + " [" + findElementIndex(loop_node) + "]", constructed_path);
+        constructed_path = assemblePathString(loop_node.localName + ":nth-child(" + findElementIndex(loop_node) + ")", constructed_path);
       }
       iterated = true; 
       loop_node = loop_node.parentNode; 
@@ -136,144 +137,37 @@ function killTrailingComma(path)
 }
 
 
-function createStringFromHashMap()
-{
-  var css_string;
-  var first_bracket = "\n{";
-  var last_bracket = "}";
+// function createStringFromHashMap()
+// {
+//   var css_string = " ";
+//   var first_bracket = "\n{";
+//   var last_bracket = "}";
 
-  if (change_map.size === 0)
-  {
-    return null; 
-  }
+//   if (change_map.size === 0)
+//   {
+//     return null; 
+//   }
 
-  for(var i = 0; i++ < change_map.size; change_map.next())
-  {
-    css_string.concat(change_map.key() + first_bracket + breakStyleChangesAndFormat(change_map.hash(change_map.key())) + last_bracket);
+//   for(var i = 0; i++ < change_map.size; change_map.next())
+//   {
+//     css_string.concat(change_map.key() + first_bracket + breakStyleChangesAndFormat(change_map.hash(change_map.key())) + last_bracket);
+//     console.log(css_string);
+//   }
 
-  }
+//   return css_string; 
+// }
 
-  return css_string; 
-}
-
-function breakStyleChangesAndFormat(style_string)
-{
-  var style_array = style_string.split(';');
-  var semicolon = ";";
-  var formatted_string_with_style = new String(); 
-  for (var i = 0; i< style_array.length i++)
-  {
-    formatted_string_with_style.concat(style_array[i], semicolon, "\n");
-  }
-  console.log(formatted_string_with_style);
-  return formatted_string_with_style; 
-}
-
-//Javascript Hashmap Implementation borrowed from this stackexchange post: http://stackoverflow.com/questions/368280/javascript-hashmap-equivalent
-function Map(linkItems) {
-    this.current = undefined;
-    this.size = 0;
-
-    if(linkItems === false)
-        this.disableLinking();
-}
-Map.noop = function() {
-    return this;
-};
-Map.illegal = function() {
-    throw new Error("illegal operation for maps without linking");
-};
-Map.from = function(obj, foreignKeys) {
-    var map = new Map;
-
-    for(var prop in obj) {
-        if(foreignKeys || obj.hasOwnProperty(prop))
-            map.put(prop, obj[prop]);
-    }
-
-    return map;
-};
-Map.prototype.disableLinking = function() {
-    this.link = Map.noop;
-    this.unlink = Map.noop;
-    this.disableLinking = Map.noop;
-    this.next = Map.illegal;
-    this.key = Map.illegal;
-    this.value = Map.illegal;
-    this.removeAll = Map.illegal;
-
-    return this;
-};
-Map.prototype.hash = function(value) {
-    return (typeof value) + ' ' + (value instanceof Object ?
-        (value.__hash || (value.__hash = ++arguments.callee.current)) :
-        value.toString());
-};
-Map.prototype.hash.current = 0;
-Map.prototype.get = function(key) {
-    var item = this[this.hash(key)];
-    return item === undefined ? undefined : item.value;
-};
-Map.prototype.put = function(key, value) {
-    var hash = this.hash(key);
-    if(this[hash] === undefined) {
-        var item = { key : key, value : value };
-        this[hash] = item;
-
-        this.link(item);
-        ++this.size;
-    }
-    else this[hash].value = value;
-    return this;
-};
-Map.prototype.remove = function(key) {
-    var hash = this.hash(key);
-    var item = this[hash];
-    if(item !== undefined) {
-        --this.size;
-        this.unlink(item);
-
-        delete this[hash];
-    }
-    return this;
-};
-Map.prototype.removeAll = function() {
-    while(this.size)
-        this.remove(this.key());
-
-    return this;
-};
-Map.prototype.link = function(item) {
-    if(this.size == 0) {
-        item.prev = item;
-        item.next = item;
-        this.current = item;
-    }
-    else {
-        item.prev = this.current.prev;
-        item.prev.next = item;
-        item.next = this.current;
-        this.current.prev = item;
-    }
-};
-Map.prototype.unlink = function(item) {
-    if(this.size == 0)
-        this.current = undefined;
-    else {
-        item.prev.next = item.next;
-        item.next.prev = item.prev;
-        if(item === this.current)
-            this.current = item.next;
-    }
-};
-Map.prototype.next = function() {
-    this.current = this.current.next;
-};
-Map.prototype.key = function() {
-    return this.current.key;
-};
-Map.prototype.value = function() {
-    return this.current.value;
-};
+// function breakStyleChangesAndFormat(style_string)
+// {
+//   var style_array = style_string.split(';');
+//   var semicolon = ";";
+//   var formatted_string_with_style= "";
+//   for (var i = 0; i< style_array.length; i++)
+//   {
+//     formatted_string_with_style.concat(style_array[i], semicolon, "\n");
+//   }
+//   console.log(formatted_string_with_style);
+//   return formatted_string_with_style; 
+// }
 
 
